290497da2c519e8fcbe0a0b69820500193b65883
Thiago Ramos <thiagoramos.al@gmail.com>
2020-11-29 18:18:34 -0300

    Deleted unused files

--- lib/gitoogl_web/live/timer_live.ex
+++ lib/gitoogl_web/live/timer_live.ex
@@ -10,14 +10,16 @@ defmodule GitooglWeb.TimerLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    started_at = Timex.now
     if connected?(socket), do: Process.send_after(self(), :update, 1000)
-    {:ok, assign(socket, time: DateTime.utc_now())}
+    {:ok, assign(socket, started_at: started_at, time: started_at)}
   end
 
   @impl true
   def handle_info(:update, socket) do
+    time_now = Timex.diff(socket.assigns.started_at, Timex.now)
     Process.send_after(self(), :update, 1000)
-    {:noreply, assign(socket, :time, DateTime.utc_now())}
+    {:noreply, assign(socket, started_at: socket.assigns.started_at, time: time_now)}
   end
 
 end
