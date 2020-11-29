9ebaa8bf46377942255e5821b0bb4abc1c449492
Thiago Ramos <thiagoramos.al@gmail.com>
2020-11-29 14:14:17 -0300

    Added timex

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Gitoogl.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:timex, "~> 3.6.2"}
     ]
   end
 
